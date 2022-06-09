34322bfe1fdd23595078ca3373b06be7b5bdf37c
Kieran Harris <kieharr.kh@gmail.com>
2022-06-09 19:53:54 +0100

    adding small order test

---
+++ CourierKata.Tests/OrderTests.cs
@@ -0,0 +1,16 @@
+﻿using CourierKata.Models;
+using NUnit.Framework;
+
+namespace CourierKata.Tests;
+
+public class OrderTests
+{
+    [Test]
+    public void It_should_print_the_correct_order_for_a_single_small_package()
+    {
+        var order = new Order();
+        order.Parcels.Add(new Parcel(1,1,1));
+        var actual = order.PrintOrder();
+        Assert.AreEqual("Small Parcel: $3. Total Cost: $3", actual);
+    }
+}
---
+++ CourierKata/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace CourierKata.Models;
+
+public class Order
+{
+    public List<Parcel> Parcels { get; set; } = new ();
+
+    public string PrintOrder()
+    {
+        throw new NotImplementedException();
+    }
+}
