a642e3749a9a5527adc23013e04688d0490f562f
Kieran Harris <kieharr.kh@gmail.com>
2022-06-09 19:38:55 +0100

    creating small parcel

--- CourierKata/Models/Parcel.cs
+++ CourierKata/Models/Parcel.cs
@@ -5,6 +5,10 @@ public class Parcel
     public ParcelSize Size { get; set; }
     public Parcel(int height, int width, int length)
     {
-        throw new NotImplementedException();
+        Size = Math.Max(height, Math.Max(width, length)) switch
+        {
+            < 10 => ParcelSize.Small,
+            _ => throw new NotImplementedException()
+        };
     }
 }
