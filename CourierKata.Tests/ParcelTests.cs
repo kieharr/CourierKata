b68359d648aeea8101dd34f25b11426e2b192bc7
Kieran Harris <kieharr.kh@gmail.com>
2022-06-09 19:41:25 +0100

    adding medium parcel test

--- CourierKata.Tests/ParcelTests.cs
+++ CourierKata.Tests/ParcelTests.cs
@@ -14,4 +14,14 @@ public class ParcelTests
         var actual = parcel.Size;
         Assert.AreEqual(ParcelSize.Small, actual);
     }
+
+    [TestCase(10,1,1)]
+    [TestCase(9,10,9)]
+    [TestCase(9,10,49)]
+    public void It_should_be_a_medium_parcel(int height, int width, int length)
+    {
+        var parcel = new Parcel(height, width, length);
+        var actual = parcel.Size;
+        Assert.AreEqual(ParcelSize.Medium, actual);
+    }
 }
